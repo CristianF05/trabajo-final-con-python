18394f9b8e80ab3dbb9a45474f3885f690426af6
CritianF05 <1482670@senati.pe>
2023-02-27 14:55:31 -0500

    Creacion de un frame para el titulo de la pagina

--- aplicacion.py
+++ aplicacion.py
@@ -6,6 +6,11 @@ class Product:
     def __init__(self, window):#para mantener un mejor codigo
         self.wind = window
         self.wind.title('sistema de registro')#creacion del nombre de la aplicacion
+        #creacion de un contenedor o frame
+        titulo = LabelFrame(self.wind, text = "Ferretería El Tornillo Feliz")
+        titulo.grid(row = 0, column = 4, columnspan = 2, pady = 20)
+
+
 #para ejecutar tinker
 if __name__ == '__main__':
     window = Tk()
