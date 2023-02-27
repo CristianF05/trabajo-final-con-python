61b8ffc591901287c38284d44881c7d6559fb5a4
CritianF05 <1482670@senati.pe>
2023-02-27 15:10:37 -0500

    Creacion de DNI para entrada de un dato

--- aplicacion.py
+++ aplicacion.py
@@ -8,9 +8,11 @@ class Product:
         self.wind.title('sistema de registro')#creacion del nombre de la aplicacion
         #creacion de un contenedor o frame
         titulo = LabelFrame(self.wind, text = "Ferretería El Tornillo Feliz")
-        titulo.grid(row = 0, column = 4, columnspan = 2, pady = 20)
-
-
+        titulo.grid(row = 0, column = 20, columnspan = 2, pady = 20)
+        #Name Imput de DNI
+        Label(titulo, text = 'DNI: ').grid(row = 1, column = 0)
+        self.name = Entry(titulo)
+        self.name.grid(row = 1, column = 4)
 #para ejecutar tinker
 if __name__ == '__main__':
     window = Tk()
