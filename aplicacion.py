15f479542446d1dd0c108512b6c3e5132b4a037f
CritianF05 <1482670@senati.pe>
2023-02-27 10:26:54 -0500

    creacion de el nomdre de la aplicacion

--- aplicacion.py
+++ aplicacion.py
@@ -1,11 +1,13 @@
-# Importar tkinter
+#Importar tkinter
 from tkinter import ttk
 from tkinter import *
-# Creacion de la clase product
+#Creacion de la clase product
 class Product:
-    def __init__(self, window):#para escrivir el codigo en esta parte
-#para ejecutar tkinter
+    def __init__(self, window):#para mantener un mejor codigo
+        self.wind = window
+        self.wind.title('sistema de registro')#creacion del nombre de la aplicacion
+#para ejecutar tinker
 if __name__ == '__main__':
-    window = tk()
+    window = Tk()
     application = Product(window)#para mantener un mejor orden
     window.mainloop()
