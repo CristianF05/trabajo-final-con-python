f98a9e01de413418137fa2abb81af7d3bdbbd1e2
CritianF05 <1482670@senati.pe>
2023-02-27 10:17:30 -0500

    comandos para ejecutar tkinter

--- aplicacion.py
+++ aplicacion.py
@@ -1,3 +1,11 @@
 # Importar tkinter
 from tkinter import ttk
 from tkinter import *
+# Creacion de la clase product
+class Product:
+    def __init__(self, window):#para escrivir el codigo en esta parte
+#para ejecutar tkinter
+if __name__ == '__main__':
+    window = tk()
+    application = Product(window)#para mantener un mejor orden
+    window.mainloop()
